0221f293d67f5bfa69ed3b2ed45c9667cd260fae
Natalia <naty-aragao@hotmail.com>
2025-05-18 22:31:16 -0300

    adição do prisma client para se conectar ao mongoDB

--- server.js
+++ server.js
@@ -1,17 +1,56 @@
 import express from 'express'
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
+const prisma = new PrismaClient();
 const app = express()
 app.use(express.json())
 
-const users = []
+app.post('/usuarios', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany()
     res.status(200).json(users)
 })
 
+app.put('/usuarios/:id', async (req, res) => {
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: "Usuário deletado com sucesso! "})
+})
+
 app.listen(3000)
+
+/*
+    MongoDB: user: naty
+    senha: TZF9d1H82U4bvJjw
+*/
